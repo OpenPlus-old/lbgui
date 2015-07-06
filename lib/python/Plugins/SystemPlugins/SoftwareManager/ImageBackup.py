b7a6f6f03e13f9f76b3694673f78e6e3c86762fd
Jglez3 <Jglez3@users.noreply.github.com>
2015-07-06 17:45:47 +0200

    [ImageBackup.py] Updated Image Version

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -18,7 +18,7 @@ import commands
 import datetime
 from boxbranding import getBoxType, getMachineBrand, getMachineName, getDriverDate, getImageVersion, getImageBuild, getBrandOEM, getMachineBuild, getImageFolder, getMachineUBINIZE, getMachineMKUBIFS, getMachineMtdKernel, getMachineKernelFile, getMachineRootFile, getImageFileSystem
 
-VERSION = "Version 4.1 openplus"
+VERSION = "Version Openplus 0.9"
 
 HaveGZkernel = True
 if getBrandOEM() in ("fulan"):
