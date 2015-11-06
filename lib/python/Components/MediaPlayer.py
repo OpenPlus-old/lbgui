e540b353764a695c7a6813a88fd121d8523f0ed4
villak <viptronick@gmail.com>
2015-11-06 14:58:14 +0100

    update skin full HD

--- lib/python/Components/MediaPlayer.py
+++ lib/python/Components/MediaPlayer.py
@@ -17,8 +17,8 @@ STATE_NONE = 5
 class PlayList(MenuList):
 	def __init__(self, enableWrapAround = False):
 		MenuList.__init__(self, [], enableWrapAround, eListboxPythonMultiContent)
-		self.l.setFont(0, gFont("Regular", 18))
-		self.l.setItemHeight(23)
+		self.l.setFont(0, gFont("Regular", 21))
+		self.l.setItemHeight(25)
 		self.currPlaying = -1
 		self.oldCurrPlaying = -1
 		self.serviceHandler = eServiceCenter.getInstance()
@@ -36,7 +36,7 @@ class PlayList(MenuList):
 		text = serviceref.getName()
 		if text is "":
 			text = path.split(serviceref.getPath().split('/')[-1])[1]
-		res.append((eListboxPythonMultiContent.TYPE_TEXT,25, 1, 470, 22, 0, RT_VALIGN_CENTER, text))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT,25, 1, 770, 22, 0, RT_VALIGN_CENTER, text))
 		try:
 			png = self.icons[state]
 			res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHATEST, 5, 3, 16, 16, png))
